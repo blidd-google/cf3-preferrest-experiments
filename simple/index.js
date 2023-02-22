d097f25ea64e8229085f5a4ecbfc7fef91dc0f90
Brian Li <blidd@google.com>
2023-02-22 17:48:04 -0500

    add consolidated logs

--- simple/index.js
+++ simple/index.js
@@ -3,6 +3,6 @@ const functions = require("firebase-functions");
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.simple = functions.https.onRequest((request, response) => {
   process.exit(1);
 });
